44a65d157da261bcd0fcebc30c3ea3d490fa8536
artem <tradegordeev@gmail.com>
2022-06-06 01:37:40 +0300

    message

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, dima!");
+Console.WriteLine("Hello, hello five times !");
+Console.WriteLine("Hello, hello five times !");
+Console.WriteLine("Hello, hello five times !");
+Console.WriteLine("Hello, hello five times !");
+Console.WriteLine("Hello, hello five times !");
+
